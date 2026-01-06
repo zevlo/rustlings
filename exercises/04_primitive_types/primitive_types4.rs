d19ad78684f918cd824f04fbdd33e11b711b496b
zevlo <zachallen21@gmail.com>
2026-01-05 22:44:58 -0500

    Complete primitive types exercises

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -9,8 +9,11 @@ mod tests {
         let a = [1, 2, 3, 4, 5];
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
-        // let nice_slice = ???
+        let nice_slice = &a[1..4];
+        assert_eq!([2, 3, 4], nice_slice);
 
+        // The upper index can be included by using the syntax `..=` (with `=` sign)
+        let nice_slice = &a[1..=3];
         assert_eq!([2, 3, 4], nice_slice);
     }
 }
