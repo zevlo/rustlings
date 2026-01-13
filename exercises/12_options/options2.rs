bf436d5138ee803ed73b0f6a748e3e19011f4c8b
zevlo <zachallen21@gmail.com>
2026-01-13 14:33:18 -0500

    Complete options exercrises

--- exercises/12_options/options2.rs
+++ exercises/12_options/options2.rs
@@ -9,8 +9,8 @@ mod tests {
         let target = "rustlings";
         let optional_target = Some(target);
 
-        // TODO: Make this an if-let statement whose value is `Some`.
-        word = optional_target {
+        // if-let
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -26,10 +26,8 @@ mod tests {
 
         let mut cursor = range;
 
-        // TODO: Make this a while-let statement. Remember that `Vec::pop()`
-        // adds another layer of `Option`. You can do nested pattern matching
-        // in if-let and while-let statements.
-        integer = optional_integers.pop() {
+        // while-let with nested pattern matching
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
